891e9f9ba15b93204d6ac2cdfeb7010ba1205a74
Cyberslave <jaymanict@gmail.com>
2023-02-21 11:59:37 +0200

    modified Datamanipulator

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -15,6 +15,7 @@ export class DataManipulator {
   static generateRow(serverRespond: ServerRespond[]): Row {
     const priceABC = (serverRespond[0].top_ask.price+serverRespond[0].top_bid.price)/2;
     const priceDEF =(serverRespond[1].top_ask.price+serverRespond[1].top_bid.price)/2;
+    const ratio = priceABC / priceDEF;
     const upperBound = 1+0.05;
     const lowerBound =1-0.05;
       return {
@@ -28,4 +29,4 @@ export class DataManipulator {
       };
     }
   }
-}
+
